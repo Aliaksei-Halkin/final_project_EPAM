06d40d20005c872cdb760900c2025236715727ce
Aliaksei Halkin <pochtax3@gmail.com>
2021-06-14 15:00:10 +0300

    added command RegisterUserCommand.java

--- src/main/java/com/epam/flyingdutchman/controller/commands/enums/CommandEnum.java
+++ src/main/java/com/epam/flyingdutchman/controller/commands/enums/CommandEnum.java
@@ -1,10 +1,10 @@
 package com.epam.flyingdutchman.controller.commands.enums;
 
 import com.epam.flyingdutchman.controller.commands.Command;
-import jakarta.servlet.http.HttpServletRequest;
+import com.epam.flyingdutchman.controller.commands.impl.RegisterUserCommand;
 
 public enum CommandEnum {
-
+    REGISTER_USER(new RegisterUserCommand());
 
     private final Command command;
 
