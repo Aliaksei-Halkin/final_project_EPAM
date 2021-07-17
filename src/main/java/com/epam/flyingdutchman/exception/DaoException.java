367d459c554ee4aa281a5626fc14489ed1e2865f
Aliaksei Halkin <pochtax3@gmail.com>
2021-07-17 21:52:25 +0300

    added 2 classes of exceptions

---
+++ src/main/java/com/epam/flyingdutchman/exception/DaoException.java
@@ -0,0 +1,38 @@
+package com.epam.flyingdutchman.exception;
+
+public class DaoException extends Exception {
+    /**
+     * Instantiates a new Dao exception.
+     */
+    public DaoException() {
+        super();
+    }
+
+    /**
+     * Instantiates a new Dao exception.
+     *
+     * @param message the message
+     */
+    public DaoException(String message) {
+        super(message);
+    }
+
+    /**
+     * Instantiates a new Dao exception.
+     *
+     * @param message   the message
+     * @param throwable the throwable
+     */
+    public DaoException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+    /**
+     * Instantiates a new Dao exception.
+     *
+     * @param throwable the throwable
+     */
+    public DaoException(Throwable throwable) {
+        super(throwable);
+    }
+}
---
+++ src/main/java/com/epam/flyingdutchman/exception/ServiceException.java
@@ -0,0 +1,38 @@
+package com.epam.flyingdutchman.exception;
+
+public class ServiceException extends Exception{
+    /**
+     * Instantiates a new Service Exception.
+     */
+    public ServiceException() {
+        super();
+    }
+
+    /**
+     * Instantiates a new Service Exception.
+     *
+     * @param message the message
+     */
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    /**
+     * Instantiates a new Service Exception.
+     *
+     * @param message   the message
+     * @param throwable the throwable
+     */
+    public ServiceException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+    /**
+     * Instantiates a new Service Exception.
+     *
+     * @param throwable the throwable
+     */
+    public ServiceException(Throwable throwable) {
+        super(throwable);
+    }
+}
