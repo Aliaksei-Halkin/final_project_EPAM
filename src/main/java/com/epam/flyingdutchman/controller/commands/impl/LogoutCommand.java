8726ee0a256cc80aa68569ebbb5a8ee87023f92f
Aliaksei Halkin <pochtax3@gmail.com>
2021-07-30 00:21:22 +0300

    fix bug

--- src/main/java/com/epam/flyingdutchman/controller/commands/impl/LogoutCommand.java
+++ src/main/java/com/epam/flyingdutchman/controller/commands/impl/LogoutCommand.java
@@ -5,15 +5,21 @@ import com.epam.flyingdutchman.util.resources.ConfigurationManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
+import java.util.Optional;
+
 import static com.epam.flyingdutchman.util.constants.Context.SESSION_LANGUAGE;
 
 public class LogoutCommand implements Command {
     @Override
     public String execute(HttpServletRequest request) {
-        HttpSession session =request.getSession();
-        String currentLanguage = (String) session.getAttribute(SESSION_LANGUAGE);
+        HttpSession session = request.getSession();
+        Optional<Object> optional = Optional.ofNullable(session.getAttribute(SESSION_LANGUAGE));
         session.invalidate();
-        session.setAttribute(SESSION_LANGUAGE,currentLanguage);
+        if (optional.isEmpty()) {
+            request.getSession().setAttribute(SESSION_LANGUAGE, "en");
+        } else {
+            request.getSession().setAttribute(SESSION_LANGUAGE, optional.get());
+        }
         return ConfigurationManager.getProperty("page.index");
     }
 }
